db3becd6b3db32e05b9ef02636534ce1f0b3d693
WataruMorioka <wtr_morioka@icloud.com>
2019-07-04 20:13:05 +0900

    Initial Commit

---
+++ FifthProject/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  FifthProject
+//
+//  Created by 森岡渉 on 2019/07/04.
+//  Copyright © 2019 森岡渉. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
